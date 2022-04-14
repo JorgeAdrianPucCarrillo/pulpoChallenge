0bf85a0e35cbd0f0b4ce6aebbd3d964c676920f2
Jorge Puc <jorge-carrillo-puc@outlook.com>
2022-04-14 16:42:20 -0500

    eliminando logs

--- controllers/dataMonetaryHelpController.js
+++ controllers/dataMonetaryHelpController.js
@@ -4,9 +4,7 @@ class dataMonetaryHelpController {
     //controlador en caso de error directo en funcion dataMonetaryHelp
     async informationRequest(req, res) {
         try{
-            console.log("llega")
             const resp = await queries.dataMonetaryHelp(req.params.countryId, req.params.year)
-            console.log(resp)
             res.status(resp.status).json(resp);
         }catch(err){
             //retorna respuesta error en caso de error causado durante la peticion o durante la funcion misma
